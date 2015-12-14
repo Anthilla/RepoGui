808953f25924fa32da7132126ca6e1b21226947a
zandam <damiano.zanardi@anthilla.com>
2015-12-14 17:59:47 +0100

    page loading optimization

--- aosrepo/Bootstrapper.cs
+++ aosrepo/Bootstrapper.cs
@@ -1,16 +1,22 @@
 ﻿using Nancy;
+using Nancy.Bootstrapper;
 using Nancy.Conventions;
+using Nancy.TinyIoc;
+using System;
 
 namespace aosrepo {
     public class Bootstrapper : DefaultNancyBootstrapper {
         protected override void ConfigureConventions(NancyConventions conv) {
             base.ConfigureConventions(conv);
-            conv.StaticContentsConventions.Add(
-                StaticContentConventionBuilder.AddDirectory("Scripts", @"/Scripts/")
-                );
-            conv.StaticContentsConventions.Add(
-                StaticContentConventionBuilder.AddDirectory("Fonts", @"/Fonts/")
-                );
+            conv.StaticContentsConventions.Clear();
+            conv.StaticContentsConventions.Add(RequestHandling.AddDirectoryWithExpiresHeader("Content", @"/Content/", TimeSpan.FromDays(365)));
+            conv.StaticContentsConventions.Add(RequestHandling.AddDirectoryWithExpiresHeader("Scripts", @"/Scripts/", TimeSpan.FromDays(365)));
+            conv.StaticContentsConventions.Add(RequestHandling.AddDirectoryWithExpiresHeader("Fonts", @"/Fonts/", TimeSpan.FromDays(365)));
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines) {
+            base.ApplicationStartup(container, pipelines);
+            pipelines.RegisterCompressionCheck();
         }
     }
 }
---
+++ aosrepo/NancyExtensions.cs
@@ -0,0 +1,102 @@
+﻿using ICSharpCode.SharpZipLib.GZip;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aosrepo {
+    public static class RequestHandling {
+        public static Func<NancyContext, string, Response> AddDirectoryWithExpiresHeader(string requestedPath, string contentPath, TimeSpan expiresTimeSpan, params string[] allowedExtensions) {
+            var responseBuilder = StaticContentConventionBuilder.AddDirectory(requestedPath, contentPath, allowedExtensions);
+            return (context, root) => {
+                var response = responseBuilder(context, root);
+                if (response != null) {
+                    response.Headers.Add("Expires", DateTime.Now.Add(expiresTimeSpan).ToString("R"));
+                    response.Headers["Transfer-Encoding"] = "chunked";
+                    response.Headers["Content-Encoding"] = "gzip";
+                    response.Headers["Cache-Control"] = "private, max-age=0";
+                    var contents = response.Contents;
+                    response.Contents = responseStream => {
+                        using (var compression = new GZipOutputStream(responseStream)) {
+                            compression.SetLevel(9);
+                            contents(compression);
+                        }
+                    };
+                }
+                return response;
+            };
+        }
+
+        public static void RegisterCompressionCheck(this IPipelines pipelines) {
+            pipelines.AfterRequest.AddItemToEndOfPipeline(CheckForCompression);
+        }
+
+        static void CheckForCompression(NancyContext context) {
+            if (!RequestIsGzipCompatible(context.Request)) {
+                return;
+            }
+
+            if (context.Response.StatusCode != HttpStatusCode.OK) {
+                return;
+            }
+
+            if (!ResponseIsCompatibleMimeType(context.Response)) {
+                return;
+            }
+
+            if (ContentLengthIsTooSmall(context.Response)) {
+                return;
+            }
+
+            CompressResponse(context.Response);
+        }
+
+        static void CompressResponse(Response response) {
+            response.Headers.Add("Expires", DateTime.Now.Add(TimeSpan.FromDays(365)).ToString("R"));
+            response.Headers["Transfer-Encoding"] = "chunked";
+            response.Headers["Content-Encoding"] = "gzip";
+            response.Headers["Cache-Control"] = "private, max-age=0";
+            response.ContentType = "text/html; charset=utf-8";
+            var contents = response.Contents;
+            response.Contents = responseStream => {
+                using (var compression = new GZipOutputStream(responseStream)) {
+                    compression.SetLevel(9);
+                    contents(compression);
+                }
+            };
+        }
+
+        static bool ContentLengthIsTooSmall(Response response) {
+            string contentLength;
+            if (response.Headers.TryGetValue("Content-Length", out contentLength)) {
+                var length = long.Parse(contentLength);
+                if (length < 4096) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static List<string> ValidMimes = new List<string> {
+                                                    "text/css",
+                                                    "text/html",
+                                                    "text/plain",
+                                                    "image/png",
+                                                    "application/xml",
+                                                    "application/json",
+                                                    "application/xaml+xml",
+                                                    "application/x-javascript",
+                                                    "application/javascript"
+                                                };
+
+        static bool ResponseIsCompatibleMimeType(Response response) {
+            return ValidMimes.Any(x => x == response.ContentType);
+        }
+
+        static bool RequestIsGzipCompatible(Request request) {
+            return request.Headers.AcceptEncoding.Any(x => x.Contains("gzip"));
+        }
+    }
+}
